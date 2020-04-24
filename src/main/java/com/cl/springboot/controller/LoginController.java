0c8c5ff725dc20fcef83f72005876eb4ab05ef2a
luluChailu <2764929386@qq.com>
2020-04-24 21:52:04 +0800

    first commit

---
+++ src/main/java/com/cl/springboot/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.cl.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
