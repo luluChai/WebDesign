1a9121e847170c6940656f3cd02045f6b05f3418
luluChailu <2764929386@qq.com>
2020-05-08 18:10:13 +0800

    修改问题展示顺序，新发布的在上面;

--- src/main/java/com/cl/springboot/mapper/QuestionMapper.java
+++ src/main/java/com/cl/springboot/mapper/QuestionMapper.java
@@ -14,7 +14,7 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
     void create(Question question);
 
-    @Select("select * from question")
+    @Select("select id,title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag from question order by id  desc")
     List<Question> list();
 
     @Select("select * from question where id = #{id}")
